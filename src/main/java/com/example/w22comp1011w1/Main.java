d6ea585054fb040f10bbcd885e3e5cf634ef43a4
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2022-01-10 10:54:22 -0500

    Added logic in the camera class.

---
+++ src/main/java/com/example/w22comp1011w1/Main.java
@@ -0,0 +1,8 @@
+package com.example.w22comp1011w1;
+
+public class Main {
+    public static void main(String[] args) {
+        Camera camera = new Camera("Canon","5D Mark IV",30,true,3099.99);
+        System.out.println(camera);
+    }
+}
